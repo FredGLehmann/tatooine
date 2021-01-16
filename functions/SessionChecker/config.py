9c2d55983ec45f5051d5add7039e062b016d17c1
Debian <admin@zoubix.net>
2021-01-16 18:20:28 +0000

    New dir orga

--- functions/SessionChecker/config.py
+++
@@ -1,4 +0,0 @@
-SESSION_TABLE = 'test-congitoSessionManager-SessionTable-13N11D33S9561'
-SESSION_TABLE_REGION = 'eu-west-3'
-COOKIE_NAME = 'Barrier'
-LOGIN_URL = 'https://d1w77mu32577ph.cloudfront.net/_identity/login'
