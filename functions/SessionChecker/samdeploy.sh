4bd0700e711ed8fca804a993af5940cc76eca548
Frederic Lehmann <fred@zoubix.net>
2021-03-21 20:37:50 +0100

    reprise

---
+++ functions/SessionChecker/samdeploy.sh
@@ -0,0 +1,1 @@
+sam deploy --stack-name sessionchecker --template-file package.yaml --region us-east-1 --parameter-overrides Stage=staging --capabilities CAPABILITY_IAM
---
+++ functions/SessionChecker/sampackage.sh
@@ -0,0 +1,1 @@
+sam package --template-file template0.yaml --s3-bucket zbx-tmp --s3-prefix lambdapkg --output-template package.yaml --region us-east-1 
